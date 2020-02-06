4c63409ef73eb0d8aca93da17c0dabd581b2cb64
Dima Kurilo <dkurilo@gmail.com>
2020-02-05 20:03:46 -0500

    separate types and functions, one more try to fix travis.yml

--- src/Lib.hs
+++ src/Lib.hs
@@ -34,8 +34,7 @@ import           Network.Wai.Handler.Warp
 import           Network.Wai.Handler.WarpTLS
 import           Servant
 import           System.Environment               (lookupEnv)
-
-type DBConnectionString = ByteString
+import           Types
 
 initConnectionPool :: DBConnectionString -> IO (Pool Connection)
 initConnectionPool connStr =
@@ -44,70 +43,6 @@ initConnectionPool connStr =
              2 -- stripes
              60 -- unused connections are kept open for a minute
              10 -- max. 10 connections open per stripe
-
-type PutAccepted = Verb 'PUT 202
-
-data ISBN = WrongISBN | ISBN10 String | ISBN13 String deriving (Eq)
-
-isbnFromString :: String -> ISBN
-isbnFromString cs | l == 10 && checkSum10ISBN cs' = ISBN10 cs
-                  | l == 13 && checkSum13ISBN cs' = ISBN13 cs
-                  | otherwise = WrongISBN
-    where cs' = filter isDigit cs
-          l = length cs'
-
-checkSum10ISBN :: String -> Bool
-checkSum10ISBN = (==0) . (`mod` 11) . foldr (\(i, c) s -> (11 - i) * (read [c] :: Int) + s) 0 . zip [1..10] . filter isDigit
-
-checkSum13ISBN :: String -> Bool
-checkSum13ISBN = (==0) . (`mod` 10) . foldr (\(i, c) s -> i * (read [c] :: Int) + s) 0 . zip oneThree . filter isDigit
-    where oneThree = join $ repeat [1,3]
-
-isbnToString :: ISBN -> String
-isbnToString (ISBN13 cs) = cs
-isbnToString (ISBN10 cs) = cs
-
-instance Show ISBN where
-    show = isbnToString
-
-instance FromJSON ISBN where
-  parseJSON v =
-    isbnFromString . unpack <$> (parseJSON v :: Parser Text)
-
-instance ToJSON ISBN where
-  toJSON (ISBN13 cs) = toJSON . pack $ cs
-  toJSON (ISBN10 cs) = toJSON . pack $ cs
-  toJSON WrongISBN   = toJSON ("" :: Text)
-
-newtype PostBook = PostBook ISBN
-
-$(deriveJSON defaultOptions ''PostBook)
-
-data Book = Book { id   :: Int
-                 , isbn :: ISBN
-                 }
-
-$(deriveJSON defaultOptions ''Book)
-
-data OverdueBook = OverdueBook { obid   :: Int
-                               , obisbn :: ISBN
-                               , obdays :: Int
-                               , obuser :: Int
-                               }
-
-instance ToJSON OverdueBook where
-    toJSON ob = object [ "id" .= (show . obid) ob
-                       , "isbn" .= (show . obisbn) ob
-                       , "daysOverdue" .= (show . obdays) ob
-                       , "user" .= (show . obuser) ob
-                       ]
-
-type BookId = Int
-
-type API = "api" :> "v1" :> "health" :> Get '[JSON] String
-      :<|> "api" :> "v1" :> "librarian" :> "books" :> ReqBody '[JSON] PostBook :> PostCreated '[JSON] Book
-      :<|> "api" :> "v1" :> "librarian" :> "books" :> ReqBody '[JSON] PostBook :> DeleteAccepted '[JSON] BookId
-      :<|> "api" :> "v1" :> "librarian" :> "books" :> "overdue" :> Get '[JSON] [OverdueBook]
 
 startApp :: IO ()
 startApp = do
